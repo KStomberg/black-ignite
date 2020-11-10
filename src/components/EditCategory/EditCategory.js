92674da38f3e0bd73cf055b52ecc060eadff1aef
Gada Wako <gada.wakooo@gmail.com>
2020-11-10 14:41:39 -0600

    nav bar for super admin

--- src/components/EditCategory/EditCategory.js
+++ src/components/EditCategory/EditCategory.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import TalkDisplay from './TalkDisplay';
-import './EditCategory.css'
+import './EditCategory.css';
+import ButtonAppBar from '../AppBar/AppBar';
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
@@ -19,6 +20,7 @@ componentDidMount() {
   render() {
     return (
       <div className="talkGallery">
+        <ButtonAppBar/>
         <h2>{this.state.heading}</h2>
         {this.props.store.talks.map(talk => 
             <TalkDisplay key={talk.id} talk={talk}/>)}
