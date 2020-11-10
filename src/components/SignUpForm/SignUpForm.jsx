023c99e105600a5b98f8bd856ba8c1eb676fe533
CooperDoten <cooper.doten@gmail.com>
2020-11-09 19:46:29 -0600

    no real changes

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {Grid} from '@material-ui/core';
 import './SignUpForm.css';
