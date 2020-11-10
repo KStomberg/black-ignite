92eb8d46c948ea1e3e85d4d299a3d4cce734f020
CooperDoten <cooper.doten@gmail.com>
2020-11-10 09:15:49 -0600

    added SQL queries for all necessary tables in DB

--- database.sql
+++ database.sql
@@ -4,7 +4,40 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+"id" SERIAL PRIMARY KEY,
+"username" VARCHAR(1000) NOT NULL,
+"password" VARCHAR(1000) NOT NULL,
+"authLevel" VARCHAR(256) NOT NULL
 );
+--create database called "Black_Ignite"
+--should be noted that some of rows such as 
+--"image_url and video_url may change depending on AWS S3 requirements
+-- in that event tables will have to be deleted in the following order 
+
+--DROP TABLE "rank";
+--DROP TABLE "submission";
+--DROP TABLE "category";
+--DROP TABLE "user";
+
+
+CREATE TABLE "category" (
+"id" SERIAL PRIMARY KEY,
+"title" VARCHAR(1000) NOT NULL,
+"description" VARCHAR(10000) NOT NULL,
+"image_url" VARCHAR(10000) NOT NULL
+);
+CREATE TABLE "submission" (
+"id" SERIAL PRIMARY KEY,
+"category_id" INT NOT NULL REFERENCES "category",
+"full_name" VARCHAR(10000) NOT NULL,
+"email" VARCHAR(10000) NOT NULL,
+"instagram" VARCHAR(5000),
+"linkedin" VARCHAR(5000), 
+"twitter" VARCHAR(5000),
+"comments" VARCHAR(50000),
+"video_url" VARCHAR(50000) NOT NULL
+);
+CREATE TABLE "rank" (
+"id" SERIAL PRIMARY KEY,
+"user_id" INT NOT NULL REFERENCES "user",
+"submission_id" INT NOT NULL REFERENCES "submission");
