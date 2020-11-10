2960ded114db7a1f9471fceab1ad0692a37d0d12
CooperDoten <cooper.doten@gmail.com>
2020-11-10 10:47:35 -0600

    added some more basic functionality

--- src/components/EditCategory/TalkDisplay.js
+++ src/components/EditCategory/TalkDisplay.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-
+import './EditCategory.css'
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
@@ -9,12 +9,24 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 class TalkDisplay extends Component {
   state = {
     heading: 'Talk Display',
+    showPopup: false
   };
+  togglePopup = () => {
+      this.setState({
+          showPopup: !this.state.showPopup
+      })
+  }
 
   render() {
     return (
-      <div>
-        <h2>{this.state.heading}</h2>
+      <div className="talkDiv">
+        <h2>{this.props.talk.title}</h2>
+        <img className="talkImages" src={this.props.talk.image_url}
+        onClick={this.togglePopup}/>
+        {this.state.showPopup ? 
+        <div className="popUpDescription">{this.props.talk.description}</div>
+      : null  
+    }  
       </div>
     );
   }
