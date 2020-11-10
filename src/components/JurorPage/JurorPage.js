05b980719f3b2db092c695eb10d59d4a06b6387b
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-11-10 11:34:50 -0600

    added sample data and sends props to a 2nd component for juror view

--- src/components/JurorPage/JurorPage.js
+++ src/components/JurorPage/JurorPage.js
@@ -2,17 +2,53 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+import JurorItem from '../JurorItem/JurorItem'
+
 import './JurorPage.css';
 
 class JurorView extends Component {
     
     state = {
-    }
+        sampleTalk: [
+                {id: 1,
+                topic: 'Redesign Our City',
+                name: 'Remi Douglass',
+                email: 'rdoug@test.com',
+                instagram: null,
+                linkedin: 'linkedin.com/user/test',
+                comment: 'this is where I put stuff and things',
+                video: 'www.blackignite.com'},
+
+                {id: 2,
+                topic: 'Allies in Action',
+                name: 'Jim Hawkins',
+                email: 'jimhawkins@aol.com',
+                instagram: '@jhawkeye',
+                linkedin: 'linkedin.com/user/test',
+                comment: 'this is where I put stuff and things',
+                video: 'www.blackignite.com'}
+            ]
+        }
+    
 
     render() {
 
         return(
-            <p>test</p>
+            <div id='topicContainer'>
+                {this.state.sampleTalk.map((submission) => (
+                    <JurorItem
+                        key={submission.id}
+                        id={submission.id}
+                        name={submission.name}
+                        email={submission.email}
+                        instagram={submission.instagram}
+                        linkedin={submission.linkedin}
+                        comment={submission.comment}
+                        video={submission.video}
+                    />
+
+                ))}
+            </div>
         )
     }
 }
