65884affe2244686e7000f45f9e55bf73073e515
CooperDoten <cooper.doten@gmail.com>
2020-11-11 14:01:56 -0600

    added appbar header to the manage juror page

--- src/components/ManageJuror/ManageJuror.js
+++ src/components/ManageJuror/ManageJuror.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import RegisterForm from '../RegisterForm/RegisterForm';
 import Swal from 'sweetalert2'
+import ButtonAppBar from '../AppBar/AppBar';
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
@@ -60,6 +61,7 @@ deleteJuror = (user) => {
   render() {
     return (
       <div>
+        <ButtonAppBar />
         <button
             type="button"
             className="btn btn_asLink"
