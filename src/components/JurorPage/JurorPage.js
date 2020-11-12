4e3767e0a29f1d1e3695e1303ba1fe69b050b920
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-11-12 10:57:59 -0600

    juror view renders match if the talkId for category and submission match

--- src/components/JurorPage/JurorPage.js
+++ src/components/JurorPage/JurorPage.js
@@ -2,62 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
-import JurorItem from '../JurorItem/JurorItem';
+import JurorTalkCategory from '../JurorTalkCategory/JurorTalkCategory'
 
 import './JurorPage.css';
 
 class JurorView extends Component {
-  state = {
-    sampleTopic: [
-      { id: 1, topicTitle: 'Redesign Our City' },
-      { id: 2, topicTitle: 'Allies in Action' },
-    ],
-    sampleTalk: [
-      {
-        id: 1,
-        topicId: 1,
-        name: 'Remi Douglass',
-        email: 'rdoug@test.com',
-        instagram: null,
-        linkedin: 'linkedin.com/user/test',
-        comment: 'this is where I put stuff and things',
-        video: 'www.blackignite.com',
-      },
 
-      {
-        id: 2,
-        topicId: 2,
-        name: 'Jim Hawkins',
-        email: 'jimhawkins@aol.com',
-        instagram: '@jhawkeye',
-        linkedin: 'linkedin.com/user/test',
-        comment: 'this is where I put stuff and things',
-        video: 'www.blackignite.com',
-      },
-    ],
-  };
+  componentDidMount() {
+    this.getTalk();
+  }
+
+  getTalk = () => {
+    console.log('Fetching talks');
+
+    this.props.dispatch({
+      type: 'FETCH_ALL_TALKS'
+    });
+  }
 
   render() {
+    console.log('this.props for jurorPage', this.props);
     return (
       <div>
         <div id="topicContainer">
-          {this.state.sampleTopic.map((topic) => (
-            <JurorItem key={topic.id} topicId={topic.id} title={topic.topicTitle} />
-          ))}
-        </div>
-        <div id="talkContainer">
-          {this.state.sampleTalk.map((submission) => (
-            <JurorItem
-              key={submission.id}
-              submissionId={submission.id}
-              topicId={submission.topicId}
-              name={submission.name}
-              email={submission.email}
-              instagram={submission.instagram}
-              linkedin={submission.linkedin}
-              comment={submission.comment}
-              video={submission.video}
-            />
+          {this.props.store.talks.map((talk) => (
+            <JurorTalkCategory key={talk.id} topicId={talk.id} title={talk.title} />
           ))}
         </div>
       </div>
