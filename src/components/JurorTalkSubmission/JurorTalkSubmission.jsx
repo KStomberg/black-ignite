f3d8562e23a406ccf0ee54b3c464315b64dfc25e
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-11-12 13:41:34 -0600

    removed excess code from jurortalksubmission.jsx

--- src/components/JurorTalkSubmission/JurorTalkSubmission.jsx
+++ src/components/JurorTalkSubmission/JurorTalkSubmission.jsx
@@ -5,43 +5,19 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 import './JurorTalkSubmission.css';
 
 class JurorTalkSubmission extends Component {
-
-    // idCompare = () => {
-    //     console.log('Testing:', this.props.topicId, this.props.categoryId);
-    //     if (this.props.topicId === this.props.categoryId) {
-    //         return (
-    //             <div>
-    //                 <p>match!</p>
-    //             </div>
-    //         )
-    //     }
-    // }
     
     render() {
         console.log('this.props for JurorTalkSubmission', this.props);
         
-        console.log('Testing:', this.props.talkId, this.props.categoryId);
-        if (this.props.talkId === this.props.categoryId) {
             return (
                 <div>
                     <p>match!</p>
                 </div>
             )
-        } else {
-            return ('')
         }
 
     }
 
 
-    // render() {
-    //     console.log('this.props for JurorTalkSubmission', this.props);
-    //     return(
-    //         <div> 
-    //         {this.idCompare}
-    //         </div>
-    //     );
-    // }
-}
 
 export default connect(mapStoreToProps)(JurorTalkSubmission);
