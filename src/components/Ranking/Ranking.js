8e00d52344b35662726fa414bbe4015e368a0270
Gada Wako <gada.wakooo@gmail.com>
2020-11-15 18:04:34 -0600

    the rendered email, instagram, linkedin, twitter and video_url are clickable links now. format is not pretty but working on it

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -20,7 +20,7 @@ class Ranking extends Component {
         return (
             <div>
                 <ButtonAppBar />
-                <div className="rankingDiv"><h2>List of Favorite Submissions</h2></div>
+                <div className="rankingDiv"><h2>Redesign our city!</h2></div>
           <table className="rankingTable">
             <thead>
             <tr>
@@ -39,11 +39,11 @@ class Ranking extends Component {
               <tr key={ranking.id}>
                    <td>{ranking.full_name}</td>
                    <a href="mailto:">{ranking.email}</a>
-                   <td>{ranking.instagram}</td>
-                   <td>{ranking.linkedin}</td>
-                   <td>{ranking.twitter}</td>
+                   <a href={ranking.instagram}>Instagram</a>
+                   <a href={ranking.linkedin}> LinkedIn Account</a>
+                   <a href={ranking.twitter}> Twitter account</a>
                    <td>{ranking.comments}</td>
-                   <td>{ranking.video_url}</td>
+                   <a href={ranking.video_url}>Video Links</a>
                    <td>{ranking.likes}</td>
                    </tr> )}
         </tbody>
