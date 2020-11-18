6b15dc40297b2eb632458eccb4b4cf7261e34b34
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-11-18 14:10:17 -0600

    styled user confirmation page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -119,17 +119,20 @@ import RegisterForm from '../RegisterForm/RegisterForm';
 import ButtonAppBar from '../AppBar/AppBar';
 import Ranking from '../Ranking/Ranking'
 import JurorPage from '../JurorPage/JurorPage'
-import SignUpFormConformation from '../SignUpForm/SignUpFormConformation';
+import SignUpFormConfirmation from '../SignUpForm/SignUpFormConfirmation';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
+          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+          <Redirect exact from="/" to="/user" />
+
           {/* User view */}
           <Route
             exact
-            path="/"
+            path="/user"
             component={Homepage}
           />
           {/* <ProtectedRoute
@@ -176,8 +179,8 @@ function App() {
 
           <Route 
             exact
-            path="/user/conformation"
-            component={SignUpFormConformation}
+            path="/user/confirmation"
+            component={SignUpFormConfirmation}
           />
 
             {/* <ProtectedRoute 
